f068968e62c0bd6b32812e51c7fb24b396e1384a
bibinbabutk7 <bibinkooikmart@gmail.com>
2024-06-05 09:48:48 +0400

    templates

--- calico/products/views.py
+++ calico/products/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    return render(request,'index.html')
+
+def about(request):
+    return render(request,'about.html')
+
+def banner(request):
+    return render(request,'blank_layout.html')
+
+def list_products(request):
+    """_summary_
+    
+    Args:
+        request (_type_): _description_
+        
+    Returns:
+        _type_: _description_
+        
+    """
+
+
+
+    return render(request,'product.html')
+
+def detail_products(request):
+    return render(request,'product.html')
